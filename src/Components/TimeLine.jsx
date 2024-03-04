83d3ed0ef54f9106c0967ef9d0a0445467806b86
PiyoPiyo4 <pio_giovanni@yahoo.com>
2024-03-05 00:21:19 +1100

    update

--- src/Components/TimeLine.jsx
+++ src/Components/TimeLine.jsx
@@ -6,10 +6,13 @@ import {
   CardMedia,
   CardContent,
   Box,
+  Divider,
 } from "@mui/material";
 import { APICall } from '../helperFunction.js'
-import { GoShieldCheck } from "react-icons/go";
-import logo from '../assets/yaallo.jpeg'
+import moment from 'moment-timezone';
+import momentTimezone from 'moment-timezone';
+import { FaRegHeart } from "react-icons/fa";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function ImageGrid() {
   let [currPage, setCurrPage] = useState(1);
@@ -31,6 +34,7 @@ export default function ImageGrid() {
       };
       // console.log('here123');
       let data = await APICall(requestBody, `/posts`, 'POST', headers);
+      console.log(data)
       if (data.length > 0) {
         setPosts([...posts, ...data]);
         setCurrPage(currPage + 1);
@@ -46,19 +50,19 @@ export default function ImageGrid() {
 
   return (
     <InfiniteScroll
-      pageStart={0}
-      loadMore={getPosts}
-      hasMore={true}
-      loader={
-        <div className="loader" key="loader">
+    pageStart={0}
+    loadMore={getPosts}
+    hasMore={true}
+    loader={
+      <div className="loader" key="loader">
           Loading ...
         </div>
       }
-    >
+      >
       <Grid container >
         {posts.map(post => (
           <Grid item key={post.time} sx={{ width: '100%', height: '100%', pb : 2}}>
-            <Image post={post}  key={post.time}/>
+            <Image post={post}  key={uuidv4()}/>
             {/* <Button onClick={() => getPosts()} sx={{ backgroundColor: 'black'}}></Button> */}
           </Grid>
         ))}
@@ -68,6 +72,20 @@ export default function ImageGrid() {
 }
 
 function Image({ post }) {
+  function getInterval(time) {
+    const isoTimeString = time.slice(0, 19);  // Extract first 19 characters
+    const backendMoment = momentTimezone.tz(isoTimeString, 'YYYY-MM-DD HH:mm:ss');
+  
+    // Get the current date and time in the user's time zone
+    const now = moment.utc().tz(momentTimezone.tz.guess());
+  
+    // Calculate the difference in milliseconds (consider time zone adjustments)
+    now.diff(backendMoment);
+  
+    // Convert milliseconds to appropriate unit and format with Moment.js
+    const formattedTime = backendMoment.fromNow();
+    return formattedTime
+  }
   return (
     <Card sx={{ 
         border: '2px solid black', 
@@ -88,14 +106,15 @@ function Image({ post }) {
       }}>
         <Box sx={{ flexDirection: 'column'}}>
           <Box sx={{ display: 'flex', justifyContent: "space-between", pl: '15px', pr: '15px'}}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Box>
-                <GoShieldCheck size= '25px' style={{ padding: 10,  }} />
+            <Box sx={{ display: 'flex', alignItems: 'center', paddingTop: 1 }}>
+              <Box sx={{ height: '25px', width: '25px', paddingRight: 2}}>
+                {/* <GoShieldCheck size= '25px' style={{ padding: 10,  }} /> */}
+                <img src={post.pp} style={{ height: '30px', width: '30px', borderRadius: '50%',}} alt="img"/>
               </Box>
               <Box sx={{ margin: 0, padding: 0, fontSize: '9.5px'}}>
-                <p style={{ margin: 0, fontSize: '10px', fontWeight: 'bold'}}>{post.brname}</p>
-                <p style={{ margin: 0}}> role</p>
-                <p style={{ margin: 0}}> Time </p>
+                <p style={{ margin: 0, fontSize: '10px', fontWeight: 'bold'}}>{post.brandName}</p>
+                <p style={{ margin: 0}}> {post.role}</p>
+                <p style={{ margin: 0}}> {getInterval(post.when)} </p>
               </Box>
             </Box>
             {/* <Box>
@@ -103,17 +122,17 @@ function Image({ post }) {
             </Box> */}
           </Box> 
           <Box>
-            <p style={{ marginLeft: 59}}> {post.title} </p>
+            <p style={{ marginLeft: 48}}> {post.title} </p>
           </Box>
           <Box>
-            {/* <p> Image </p> */}
-            <img src={logo} alt="img"/>
+            <img src={post.image} alt="img" style={{ width: '100%'}}/>
           </Box>
           <Box sx={{display: 'flex', gap: '1.5rem', ml: 3}}>
-            <p> Like count </p>
-            <p> Share </p>
-            <p> Comment </p>
+            <p style={{ fontSize: '13px'}}> <FaRegHeart color="red"/> {post.likes}</p>
+            <p style={{ fontSize: '13px'}}> Share </p>
+            <p style={{ fontSize: '13px'}}> Comment </p>
           </Box>
+          <Divider variant="middle"  component="div" sx={{ borderColor: 'black'}}/>
           <Box sx={{display: 'flex', gap: '1.5rem', ml: 3}}>
             <p> Like count </p>
             <p> Share </p>
--- src/Pages/DesktopContent.jsx
+++ src/Pages/DesktopContent.jsx
@@ -4,9 +4,27 @@ import {Box, Grid, Paper, Button} from '@mui/material';
 import RightLogin from '../assets/rightvector.svg'
 import LeftLogin from '../assets/leftvector.svg'
 import TimeLine from '../Components/TimeLine';
+// import { APICall } from '../helperFunction.js'
 
 const DesktopContent = () => {
   const navigate = useNavigate();
+  // async function dataApp() {
+  //   // let fetchedData = [{}];
+  //   try {
+  //     const headers = {
+  //       'Content-Type': 'application/json',
+  //     };
+  //     const requestBody = {
+  //       pageSize: 1,
+  //     };
+  //     // console.log('here123');
+  //     let data = await APICall(requestBody, `/add`, 'POST', headers);
+  //     console.log(data)
+  //   } catch (err) {
+  //     console.log(err);
+  //     alert(err);
+  //   }
+  // }
   return (
   <div style={{ overflow: 'hidden'}}>
     <Grid container spacing={2} sx={{mt: 2, width: '100%'}}>
@@ -49,6 +67,7 @@ const DesktopContent = () => {
       <Grid item xs={12} sm={4}>
         <Paper sx={{ height: '100%' }}>
           <Box sx={{justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            {/* <Button onClick={() => dataApp()} sx={{ backgroundColor: 'black'}}></Button> */}
             <TimeLine />
           </Box>
         </Paper>
